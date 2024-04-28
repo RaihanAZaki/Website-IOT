ecf2d1e9ebf9e2a3ba9675e53959e121b2470ed2
Raihan <89891935+RaihanAZaki@users.noreply.github.com>
2024-04-28 10:25:27 +0700

    Update TabelPerangkat.jsx

--- src/components/Tabel/TabelPerangkat.jsx
+++ src/components/Tabel/TabelPerangkat.jsx
@@ -34,19 +34,19 @@ function TabelPerangkat() {
             <div className='p-2'>
               <table className=''>
                 <tr className='text-center text-xs md:text-sm border-2 border-b-DarkPurple '>
-                  <th className='px-2 py-2 w-12 '> No </th>
-                  <th className='px-2 py-2 w-36'> Kode Perangkat </th>
-                  <th className='px-2 py-2 w-36'> Nama Perangkat </th>
-                  <th className='px-2 py-2 w-24 hidden md:block'> Gambar </th>
-                  <th className='px-2 py-2 w-36'> status </th>
+                  <th className='px-1 md:px-2 py-2 w-12 '> No </th>
+                  <th className='px-1 md:px-2 py-2 w-36'> Kode Perangkat </th>
+                  <th className='px-1 md:px-2 py-2 w-48'> Nama Perangkat </th>
+                  <th className='px-1 md:px-2 py-2 w-24 hidden md:block'> Gambar </th>
+                  <th className='px-1 md:px-2 py-2 w-36'> Status </th>
                 </tr>
                 {Data.map((data, index) => ( 
                 <tr key={index} className='text-center text-xs md:text-sm border-2 border-b-DarkPurple '>
-                  <td className='px-2 py-2 w-12 '> {data.no} </td>
-                  <td className='px-2 py-2 w-36'> {data.kode} </td>
-                  <td className='px-2 py-2 w-36'> {data.nama} </td>
-                  <td className='px-2 py-2 w-24 items-center hidden md:block justify-center'> <img src={data.gambar} className='w-10 h-10' alt="Gambar" /> </td>
-                  <td className="px-2 py-2 w-36"><p className={`${data.status === 0 ? 'bg-red-600' : data.status === 1 ? 'bg-green-600' : ''} text-white p-1 md:p-2 rounded-md text-xs`}> {data.status === 1 ? 'Aktif' : 'Tidak Aktif'} </p> </td>
+                  <td className='px-1 md:px-2 py-2 w-12 '> {data.no} </td>
+                  <td className='px-1 md:px-2 py-2 w-36'> {data.kode} </td>
+                  <td className='px-1 md:px-2 py-2 w-48'> {data.nama} </td>
+                  <td className='px-1 md:px-2 py-2 w-24 items-center hidden md:block justify-center'> <img src={data.gambar} className='w-10 h-10' alt="Gambar" /> </td>
+                  <td className="px-1 md:px-2 py-2 w-36"><p className={`${data.status === 0 ? 'bg-red-600' : data.status === 1 ? 'bg-green-600' : ''} text-white p-1 rounded-md text-xs`}> {data.status === 1 ? 'Aktif' : 'Tidak Aktif'} </p> </td>
                 </tr>
               ))}
               </table>

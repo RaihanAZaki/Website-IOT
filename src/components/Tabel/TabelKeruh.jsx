f89216ca7a86ef9906e7f96ce36fab566d3e315b
Raihan <89891935+RaihanAZaki@users.noreply.github.com>
2024-04-28 10:29:27 +0700

    Update TabelKeruh.jsx

--- src/components/Tabel/TabelKeruh.jsx
+++ src/components/Tabel/TabelKeruh.jsx
@@ -9,17 +9,6 @@ function TabelKeruh() {
       nilai: "250",
       tingkat: "Normal"
     },
-    {import React from 'react'
-
-function TabelKeruh() {
-
-  const Data = [
-    {
-      no: 1,
-      tanggal: "25/04/2024",
-      nilai: "250",
-      tingkat: "Normal"
-    },
     {
       no: 2,
       tanggal: "26/04/2024",
@@ -65,60 +54,6 @@ function TabelKeruh() {
                   <td className='px-1 md:px-2 py-2 w-36'> {data.tanggal} </td>
                   <td className='px-1 md:px-2 py-2 w-36'> {data.nilai} </td>
                   <td className='px-1 md:px-2 py-2 w-96'> {data.tingkat} </td>
-                </tr>
-              ))}
-              </table>
-            </div>
-        </div>
-    </div>
-  )
-}
-
-export default TabelKeruh
-      no: 2,
-      tanggal: "26/04/2024",
-      nilai: "270",
-      tingkat: "Normal"
-    },
-    {
-      no: 3,
-      tanggal: "27/04/2024",
-      nilai: "450",
-      tingkat: "Sedang"
-    },
-    {
-      no: 4,
-      tanggal: "28/04/2024",
-      nilai: "750",
-      tingkat: "Tinggi"
-    },
-    {
-      no: 5,
-      tanggal: "29/04/2024",
-      nilai: "782",
-      tingkat: "Tinggi"
-    },
-  ]
-  return (
-    <div className='col-span-1 pt-8 '>
-        <div className='rounded-md border-2 border-DarkPurple p-4 w-full'>
-            <div className='flex justify-center'>
-                <h1 className='text-DarkPurple font-bold text-lg md:text-xl mb-4'>Tabel Kekeruhan</h1>
-            </div>
-            <div className='p-2'>
-              <table className=''>
-                <tr className='text-center text-xs md:text-sm border-2 border-b-DarkPurple'>
-                  <th className='px-2 py-2 w-16'> No </th>
-                  <th className='px-2 py-2 w-36'> Tanggal </th>
-                  <th className='px-2 py-2 w-36'> Nilai </th>
-                  <th className='px-2 py-2 w-96'> Tingkat Keruh </th>
-                </tr>
-                {Data.map((data, index) => ( 
-                <tr key={index} className='text-center text-xs md:text-sm border-2 border-b-DarkPurple'>
-                  <td className='px-2 py-2 w-16'> {data.no} </td>
-                  <td className='px-2 py-2 w-36'> {data.tanggal} </td>
-                  <td className='px-2 py-2 w-36'> {data.nilai} </td>
-                  <td className='px-2 py-2 w-96'> {data.tingkat} </td>
                 </tr>
               ))}
               </table>

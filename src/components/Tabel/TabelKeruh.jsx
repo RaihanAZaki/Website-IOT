9a3c62b659d27a5f2eca7a9c809a3087b5b8789b
Raihan <89891935+RaihanAZaki@users.noreply.github.com>
2024-04-28 09:50:39 +0700

    Add files via upload

---
+++ src/components/Tabel/TabelKeruh.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function TabelKeruh() {
+
+  const Data = [
+    {
+      no: 1,
+      tanggal: "25/04/2024",
+      nilai: "250",
+      tingkat: "Normal ( Air Bersih )"
+    },
+    {
+      no: 2,
+      tanggal: "26/04/2024",
+      nilai: "270",
+      tingkat: "Normal ( Air Bersih )"
+    },
+    {
+      no: 3,
+      tanggal: "27/04/2024",
+      nilai: "450",
+      tingkat: "Sedang ( Tetap Waspada!! )"
+    },
+    {
+      no: 4,
+      tanggal: "28/04/2024",
+      nilai: "750",
+      tingkat: "Tinggi ( Mesin  Bergerak )"
+    },
+    {
+      no: 5,
+      tanggal: "29/04/2024",
+      nilai: "782",
+      tingkat: "Tinggi ( Mesin  Bergerak )"
+    },
+  ]
+  return (
+    <div className='col-span-1 pt-8 '>
+        <div className='rounded-md border-2 border-DarkPurple p-4 w-full'>
+            <div className='flex justify-center'>
+                <h1 className='text-DarkPurple font-bold text-lg md:text-xl mb-4'>Tabel Kekeruhan</h1>
+            </div>
+            <div className='p-2'>
+              <table className=''>
+                <tr className='text-center text-sm border-2 border-b-DarkPurple'>
+                  <th className='px-2 py-2 w-16'> No </th>
+                  <th className='px-2 py-2 w-36'> Tanggal </th>
+                  <th className='px-2 py-2 w-36'> Nilai </th>
+                  <th className='px-2 py-2 w-96'> Tingkat Keruh </th>
+                </tr>
+                {Data.map((data, index) => ( 
+                <tr key={index} className='text-center text-sm border-2 border-b-DarkPurple'>
+                  <td className='px-2 py-2 w-16'> {data.no} </td>
+                  <td className='px-2 py-2 w-36'> {data.tanggal} </td>
+                  <td className='px-2 py-2 w-36'> {data.nilai} </td>
+                  <td className='px-2 py-2 w-96'> {data.tingkat} </td>
+                </tr>
+              ))}
+              </table>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TabelKeruh
---
+++ src/components/Tabel/TabelPerangkat.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function TabelPerangkat() {
+
+  const Data = [
+    {
+      no: 1,
+      kode: "KP-A01",
+      nama: "Ultrasonic",
+      gambar: "../assets/image/ultrasonic.png",
+      status: 1,
+    },
+    {
+      no: 2,
+      kode: "KP-B02",
+      nama: "Turbidity",
+      gambar: "../assets/image/turbidity.png",
+      status: 1,
+    },
+    {
+      no: 3,
+      kode: "KP-A03",
+      nama: "Kamera",
+      gambar: "../assets/image/kamera.png",
+      status: 0,
+    },
+  ]
+  return (
+    <div className='col-span-1 pt-8 '>
+        <div className='rounded-md border-2 border-DarkPurple p-4 w-full'>
+            <div className='flex justify-center'>
+                <h1 className='text-DarkPurple font-bold text-lg md:text-xl mb-4'>Tabel Perangkat</h1>
+            </div>
+            <div className='p-2'>
+              <table className='max-h-[500px] overflow-y-scroll'>
+                <tr className='text-center text-sm border-2 border-b-DarkPurple '>
+                  <th className='px-2 py-2 w-12 '> No </th>
+                  <th className='px-2 py-2 w-36'> Kode Perangkat </th>
+                  <th className='px-2 py-2 w-48'> Nama Perangkat </th>
+                  <th className='px-2 py-2 w-24'> Gambar </th>
+                  <th className='px-2 py-2 w-36'> status </th>
+                </tr>
+                {Data.map((data, index) => ( 
+                <tr key={index} className='text-center text-sm border-2 border-b-DarkPurple '>
+                  <td className='px-2 py-2 w-12 '> {data.no} </td>
+                  <td className='px-2 py-2 w-48'> {data.kode} </td>
+                  <td className='px-2 py-2 w-48'> {data.nama} </td>
+                  <td className='px-2 py-2 w-24 flex items-center justify-center'> <img src={data.gambar} className='w-10 h-10' alt="Gambar" /> </td>
+                  <td className="px-2 py-2 w-24"><p className={`${data.status === 0 ? 'bg-red-600' : data.status === 1 ? 'bg-green-600' : ''} text-white p-2 rounded-md text-xs`}> {data.status === 1 ? 'Aktif' : 'Tidak Aktif'} </p> </td>
+                </tr>
+              ))}
+              </table>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TabelPerangkat

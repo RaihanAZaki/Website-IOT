114628dff8dea7bed2f5f44d7d000ac8e2ecdc18
Raihan <89891935+RaihanAZaki@users.noreply.github.com>
2024-04-30 04:51:44 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,69 @@ import CardCamera2 from "./components/Cards/CardCamera2";
 const queryClient = new QueryClient();
 
 function App() {
+  const Data = [
+    {
+      no: 1,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "263",
+    },
+    {
+      no: 2,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "288",
+    },
+    {
+      no: 3,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "264",
+    },
+    {
+      no: 4,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "1184",
+    },
+    {
+      no: 5,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "288",
+    },
+    {
+      no: 6,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "1184",
+    },
+    {
+      no: 7,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "48",
+    },
+    {
+      no: 8,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "18",
+    },
+    {
+      no: 9,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "192",
+    },
+    {
+      no: 10,
+      kode: "KP-A01",
+      tanggal: "2024-04-29",
+      jarak: "8",
+    },
+  ]
+  
   return (
     <QueryClientProvider client={queryClient}>
       <Layouts>
@@ -23,7 +86,7 @@ function App() {
           </div>
           <div className="md:grid grid-cols-2 gap-4 mb-8">
             <TabelPerangkat/>
-            <TabelDesign />
+            <TabelDesign data={Data}/>
           </div>
         </div>
       </Layouts>

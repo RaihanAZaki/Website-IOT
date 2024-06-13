3db0f9e26b50e3d63ee5583769aeef66529caa14
Raihan <89891935+RaihanAZaki@users.noreply.github.com>
2024-06-13 22:43:28 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import {QueryClientProvider, QueryClient} from "@tanstack/react-query"
 import Layouts from "./components/Layouts";
 import SidebarMenu from "./components/Sidebar/SidebarMenu";
-import CardGrafik from "./components/Cards/CardGrafik";
 import CardCamera from "./components/Cards/CardCamera";
 import TabelUltrasonic from "./components/Tabel/TabelPerangkat";
-import TabelDesign from "./components/Tabel/TabelDesign";
-import CardGrafik2 from "./components/Cards/CardGrafik2";
-import CardCamera2 from "./components/Cards/CardCamera2"
+import CardGrafikDashboard from "./components/Cards/CardGrafikDashboard";
+import TabelKeruh from "./components/Tabel/TabelTurbidity";
+
 
 function App() {
   return (
@@ -16,12 +15,12 @@ function App() {
         <SidebarMenu />
         <div className="w-full px-8">
           <div className="md:grid grid-cols-3 gap-4">
-            <CardGrafik2 />
+            <CardGrafikDashboard />
             <CardCamera />
           </div>
           <div className="md:grid grid-cols-2 gap-4 mb-8">
             <TabelUltrasonic />
-            <TabelDesign data={Data}/>
+            <TabelKeruh/>
           </div>
         </div>
       </Layouts>
